cd6774a889447774b1fd97e9f217ccfd512d1895
Devon Fulcher <devonfulcher3@gmail.com>
2020-05-31 18:57:50 -0500

    two sums complete

--- Two Sums/challenge.py
+++ Two Sums/challenge.py
@@ -1,1 +1,14 @@
 # https://leetcode.com/problems/two-sum/
+
+'''
+target = 9
+nums = [2, 7, 11, 15]
+'''
+
+def twoSums(nums, target):
+    numToIndex = {}
+    for i, num in enumerate(nums):
+        numToIndex[num] = i
+    for i, num in enumerate(nums):
+        if target-num in numToIndex.keys() and i != numToIndex[target-num]:
+            return [i, numToIndex[target-num]]
