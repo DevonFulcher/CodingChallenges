6ddf2aa3950340b2775dc18d1cfcce33e9fd7691
DevonFulcher <devonfulcher3@gmail.com>
2022-02-17 12:13:28 -0600

    findingAllAnagramsInAString

---
+++ Sliding Window/findAllAnagramsInAString.py
@@ -0,0 +1,19 @@
+from collections import deque, Counter
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        l, r = 0, len(p)
+        window_counter = Counter(deque(s[l:r]))
+        result = []
+        p_counter = Counter(p)
+        while r < len(s):
+            if window_counter == p_counter:
+                result.append(l)
+            window_counter[s[l]] -= 1
+            window_counter[s[r]] += 1
+            l += 1
+            r += 1
+
+        if window_counter == p_counter:
+            result.append(l)
+        return result
